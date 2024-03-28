fd33ae89f7bf5f99441c8d98fd095062e553ed0b
Tim <superibm@mail.ru>
2024-03-28 15:27:28 +1000

    GCR-site-16 Реализовать модуль подборок курсов

---
+++ src/pages/dashboard/collections/Create.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="content">
+    <Breadcrumb class="content__breadcrumbs">
+      <Item
+        href=""
+        @click="onClickBreadcrumbItem('Dashboard')"
+      >
+        <Lang value="dashboard.home" />
+      </Item>
+      <Item
+        href=""
+        @click="onClickBreadcrumbItem('Collections')"
+      >
+        <Lang value="collection.name" />
+      </Item>
+      <Item>
+        <Lang value="collection.addCollection" />
+      </Item>
+    </Breadcrumb>
+    <FormCreate />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Breadcrumb from 'ant-design-vue/lib/breadcrumb';
+import { useMeta } from 'vue-meta';
+import { useRouter } from 'vue-router';
+
+import Lang from '@/components/atoms/Lang.vue';
+import FormCreate from '@/components/modules/collection/organisms/FormCreate.vue';
+import lang from '@/helpers/lang';
+
+const {
+  Item,
+} = Breadcrumb;
+
+const router = useRouter();
+
+useMeta({
+  title: lang('collection.addCollection'),
+});
+
+const onClickBreadcrumbItem = async (name: string) => {
+  await router.push({
+    name,
+  });
+};
+</script>
---
+++ src/pages/dashboard/collections/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="content">
+    <Breadcrumb class="content__breadcrumbs">
+      <Item
+        href=""
+        @click="onClickBreadcrumbItem('Dashboard')"
+      >
+        <Lang value="dashboard.home" />
+      </Item>
+      <Item>
+        <Lang value="collection.name" />
+      </Item>
+    </Breadcrumb>
+    <Content class="content__panel">
+      <Table />
+    </Content>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Breadcrumb from 'ant-design-vue/lib/breadcrumb';
+import Layout from 'ant-design-vue/lib/layout';
+import { useMeta } from 'vue-meta';
+import { useRouter } from 'vue-router';
+
+import Lang from '@/components/atoms/Lang.vue';
+import Table from '@/components/modules/collection/organisms/Table.vue';
+import lang from '@/helpers/lang';
+
+const {
+  Content,
+} = Layout;
+
+const {
+  Item,
+} = Breadcrumb;
+
+const router = useRouter();
+
+useMeta({
+  title: lang('collection.name'),
+});
+
+const onClickBreadcrumbItem = async (name: string) => {
+  await router.push({
+    name,
+  });
+};
+</script>
